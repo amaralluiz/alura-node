f0c2854299d954d56d5a5ebf4dbc5634237a99b4
Luiz Amaral <alaluizamaral@gmail.com>
2021-07-26 10:21:48 -0300

    Criacao de endpoint de listagem de todos atendimentos. Criacao de endpoint de busca de atendimento por id

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const { query } = require('../infraestrutura/conexao');
 const conexao = require('../infraestrutura/conexao');
 
 
@@ -6,16 +7,66 @@ class Atendimento {
   adiciona(atendimento, res) {
     const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
-    const atendimentoDatado = {...atendimento, dataCriacao, data};
-    const sql = 'INSERT INTO Atendimentos SET ?';
+    
+    const dataValidada = moment(data).isSameOrAfter(dataCriacao);
+    const clienteValidado = atendimento.cliente.length >= 5;
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataValidada,
+        mensagem: 'Data deve ser maior ou igual a data atual'
+      },
+      {
+        nome: 'cliente',
+        valido: clienteValidado,
+        mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+      }
+    ];
+
+    const erros = validacoes.filter(campo => !campo.valido);
+    const existemErros = erros.length;
+
+    if(existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = {...atendimento, dataCriacao, data};
+      const sql = 'INSERT INTO Atendimentos SET ?';
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if(erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
+    
+  }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos';
+
+    conexao.query(sql, (erro, resultados) => {
+      if(erro){
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  }
+
+  buscaPorId(id, res) {
+    
+    const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      const atendimento = resultados[0];
       if(erro) {
         res.status(400).json(erro);
       } else {
-        res.status(201).json(resultados);
+        res.status(200).json(atendimento);
       }
-
     });
   }
 }
