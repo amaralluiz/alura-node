be704db4278b79aa9c4db031a181bda8bf5ce839
Luiz Amaral <alaluizamaral@gmail.com>
2021-07-27 16:41:15 -0300

    Tratamento de erro para caso imagem vier em formato invalido

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
-  const novoCaminho = `./assets/imagens/${nomeDoArquivo}`;
-  fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho));
+  const tiposValidos = ['jpg', 'png', 'jpeg'];
+  const tipo = path.extname(caminho);
+  const tipoValidado = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+  if(tipoValidado) {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(false, novoCaminho));
+  } else {
+    const erro = "Tipo é inválido";
+    console.log('Erro! Tipo Inválido');
+    callbackImagemCriada(erro);
+  }
 }
